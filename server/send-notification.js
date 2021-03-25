55c40c05df37bda03dc415d1e26bf843aa82fad6
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-25 13:25:16 -0300

    criando rota para envio de notificação

---
+++ server/send-notification.js
@@ -0,0 +1,26 @@
+const webpush = require('web-push');
+const { simpleNotification } = require('./notifications/simple-notification');
+const { subscriptions } = require("./db/subscriptions");
+
+exports.sendNotification = async (req, res) => {
+
+  const notificationPayload = simpleNotification;
+
+  // console.log(webpush.generateVAPIDKeys());
+  const vapidDetails = {
+    subject: 'seu-email@mail.com',
+    publicKey: 'BBl5Vw0PCEM8nbonAjahMaBPAR3MEibrU-zwkXHd0vp_bL4w43ej_K41pLBWOIjCW_3TnotZvskdY_Xmg0Hde3I',
+    privateKey: 'QHznI0Lrhm5c8ByTsuNyuJKZamqo7qSXwuyBfSD7sIs'
+  }
+
+  for (const subscription of subscriptions) {
+    await webpush.sendNotification(
+      subscription,
+      JSON.stringify(notificationPayload),
+      vapidDetails
+    );
+  }
+
+  res.status(200).json({message: 'Notificações enviadas!'});
+}
+
