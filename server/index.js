75178bd7aebfc35719f503b6ff80da73cc81d0d1
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-25 13:25:37 -0300

    chamando rotas do servidor

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { addSubscription } = require('./add-subscription');
+const { sendNotification } = require('./send-notification');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.route('/add-subscription').post(addSubscription);
+app.route('/send-notification').post(sendNotification);
 
 app.listen(9000, () =>
   console.log('Servidor de Push Notifications rodando!')
