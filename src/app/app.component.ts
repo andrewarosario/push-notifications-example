0cfa9e3d71290ea3eb38d6f6ab4a6367e9099829
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-28 19:45:42 -0300

    implementação de push no angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'push-notifications-example';
+export class AppComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private swPush: SwPush
+  ) {
+    this.swPush.requestSubscription({
+      serverPublicKey: 'BBl5Vw0PCEM8nbonAjahMaBPAR3MEibrU-zwkXHd0vp_bL4w43ej_K41pLBWOIjCW_3TnotZvskdY_Xmg0Hde3I'
+    })
+    .then(subscription => {
+      console.log('Usuário permitiu notificações.');
+      this.http.post('http://localhost:9000/add-subscription', subscription)
+        .subscribe();
+    })
+    .catch(err => {
+      console.error('Usuário recusou ou Navegador não suporta', err);
+    });
+  }
+
+  ngOnInit(): void {
+    this.subscribeToNotificationClicks();
+  }
+
+  subscribeToNotificationClicks(): void {
+    this.swPush.notificationClicks
+      .subscribe(result => {
+        alert('Usuário clicou na ação ' + result.action);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
