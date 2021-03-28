16267489afb6a267adefd44f46229eb36620b081
Andrew Rosário <andrew.arosario@gmail.com>
2021-03-28 19:44:36 -0300

    mudando rota de envio para get

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.route('/add-subscription').post(addSubscription);
-app.route('/send-notification').post(sendNotification);
+app.route('/send-notification').get(sendNotification);
 
 app.listen(9000, () =>
   console.log('Servidor de Push Notifications rodando!')
